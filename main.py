8e77eeed5b51f859c8eb2cd889582c232d87525e
Hananeh Shamasi <H.Shamasi@Jabama.com>
2026-07-26 01:37:52 +0330

    Add Render web server

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 from dotenv import load_dotenv
 import os
+import threading
+from flask import Flask
 
 load_dotenv()
 
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 
-TOKEN =os.getenv("BOT_TOKEN")
+TOKEN = os.getenv("BOT_TOKEN")
+
+app_web = Flask(__name__)
+
+@app_web.route("/")
+def home():
+    return "Bot is running!"
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    app_web.run(host="0.0.0.0", port=port)
 
 async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("I got your message! 🎉")
 
-app = ApplicationBuilder().token(TOKEN).build()
+def run_bot():
+    bot = ApplicationBuilder().token(TOKEN).build()
+    bot.add_handler(MessageHandler(filters.TEXT, reply))
+    print("Bot is running...")
+    bot.run_polling()
 
-app.add_handler(MessageHandler(filters.TEXT, reply))
-
-print("Bot is running...")
-
-app.run_polling()
+threading.Thread(target=run_web).start()
+run_bot()
